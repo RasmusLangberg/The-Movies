889a182931acc7048f331a3dfd0a69babf9541d1
RasmusLangberg <rsla79145@edu.ucl.dk>
2026-08-22 17:30:54 +0200

    tilføj noter til forklaring

--- The Movies/ViewModel/MovieViewModel.cs
+++ The Movies/ViewModel/MovieViewModel.cs
@@ -13,25 +13,41 @@ namespace The_Movies.ViewModel
     public class MovieViewModel : INotifyPropertyChanged
     {
         // vi bruger IMovieRepository interface til at definere en kontrakt for MovieRepository-klassen, som gør det muligt at ændre implementeringen af MovieRepository uden at ændre MovieViewModel-klassen.
+        // ViewModel'en bruger interfacet som en kontrakt til Repository.
+        // Det betyder, at ViewModel'en ikke behøver vide, hvordan data bliver gemt.
 
         private readonly IMovieRepository _repo;
 
+
+        // ObservableCollection indeholder de film, som View/ListBox skal vise.
+        // ObservableCollection sørger for, at WPF automatisk opdager,
+        // når en film bliver tilføjet eller fjernet.
         public ObservableCollection<Movie> Movies { get; set; }
 
+
+        // Command som bliver bundet til "Tilføj"-knappen i XAML.
         public ICommand AddMovieCommand { get; }
 
+
+
+
+        // Command som bliver bundet til "Slet"-knappen i XAML.
         public ICommand RemoveMovieCommand { get; }
 
 
 
-
+        // Privat felt som gemmer den titel, brugeren skriver i TextBox.
         private string _title;
+
+        // Property som TextBox'en binder til.
+        // Når værdien ændres, sender OnPropertyChanged besked til WPF.
         public string Title
         {
             get => _title;
             set
             {
                 _title = value;
+                
                 OnPropertyChanged(nameof(Title));
             }
         }
@@ -57,9 +73,15 @@ namespace The_Movies.ViewModel
                 OnPropertyChanged(nameof(Genre));
             }
         }
-
+        // Privat felt som holder den Movie, brugeren har valgt i ListBox.
         private Movie _selectedMovie;
 
+
+        // SelectedItem fra ListBox'en bindes til denne property.
+        // Den gør det muligt for ViewModel'en at vide,
+        // hvilken film brugeren har valgt.
+
+        // SelectedItem er en indbygget funktion i Listbox der gør man kan klikke på de oprettet film på listen 
         public Movie SelectedMovie
         {
             get { return _selectedMovie; }
@@ -69,18 +91,28 @@ namespace The_Movies.ViewModel
                 OnPropertyChanged(nameof(SelectedMovie));
             }
         }
-            
 
 
+        // Constructoren bliver kørt, når MovieViewModel bliver oprettet.
+        // Repository bliver sendt ind udefra og gemt i _repo.
         public MovieViewModel(IMovieRepository repo)
         {
+
+            // Gemmer det Repository, som ViewModel'en skal bruge.
             _repo = repo;
 
+
+            // Henter alle eksisterende film fra Repository.
+            // Filmene lægges ind i ViewModel'ens ObservableCollection.
             Movies = new ObservableCollection<Movie>(_repo.GetAllMovies());
 
-
+            // Opretter commanden til Tilføj-knappen.
+            // Når commanden bliver kørt, kaldes AddMovie().
             AddMovieCommand = new RelayCommand(parameter => AddMovie());
 
+
+            // Opretter commanden til Slet-knappen.
+            // Når commanden bliver kørt, kaldes RemoveMovie().
             RemoveMovieCommand = new RelayCommand(parameter => RemoveMovie());
 
 
@@ -119,8 +151,12 @@ namespace The_Movies.ViewModel
 
 
 
-
+        // Event som bruges af INotifyPropertyChanged.
+        // WPF kan dermed få besked, når en property ændrer sig.
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        // Denne metode sender besked til WPF om,
+        // at en bestemt property har ændret sig. uden PropertyChangedEventArgs ville WPF ikke opdatere det bruger ser på skærmen selvom værdien ædrede sig
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(
