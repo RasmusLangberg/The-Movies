e7238dd1d5ea84502e6dbd45aba6eb3b2a7b24e3
Thomas Schjerlund <thomas@u-man.dk>
2026-08-22 10:24:04 +0200

    Adds CRUD to repository

--- The Movies/Model/Repository/MovieRepository.cs
+++ The Movies/Model/Repository/MovieRepository.cs
@@ -13,14 +13,37 @@ namespace The_Movies.Model.Repository
         {
             _movies = new List<Movie>();
         }
+        // Create: Adds a new movie to the repository
         public void AddMovie(Movie movie)
         {
             _movies.Add(movie);
         }
+
+        // Read: Retrieves a movie by its title
+        public Movie GetMovieByTitle(string title)
+        {
+            return _movies.Find(m => m.Title.Equals(title, StringComparison.OrdinalIgnoreCase));
+        }
+
+        // Update: Updates an existing movie in the repository
+        public void UpdateMovie(Movie movie)
+        {
+            var existingMovie = GetMovieByTitle(movie.Title);
+            if (existingMovie != null)
+            {
+                existingMovie.Title = movie.Title;
+                existingMovie.Length = movie.Length;
+                existingMovie.Genre = movie.Genre;
+            }
+        }
+
+        // Delete: Removes a movie from the repository
         public void RemoveMovie(Movie movie)
         {
             _movies.Remove(movie);
         }
+
+        // Retrieves all movies in the repository
         public List<Movie> GetAllMovies()
         {
             return _movies;
